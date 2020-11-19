4a2049a64ca8b8aefc818f5fb1fa585a120abccc
upakar <upakarpaudel96@gmail.com>
2020-11-19 12:28:21 -0700

    Adding Serverless API

---
+++ list.js
@@ -0,0 +1,20 @@
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+    const params = {
+        TableName: process.env.tableName,
+        // 'KeyConditionExpression' defines the condition for the query
+        // - 'userid = :userid': only return items with matching 'userid' partition key
+        KeyConditionExpression: "userid = :userid",
+        // 'ExpressionAttributeValues' defines the value in the condition
+        // -':userid': defines 'userid' to be the id of the author
+        ExpressionAttributeValues: {
+            ":userid": "123",
+        },
+    };
+
+    const result = await dynamoDb.query(params);
+
+    return result.Items;
+});
